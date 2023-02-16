9852d2bf8c7533ca096546927f5d00c592a0423c
damir <d_kulzhabai@kbtu.kz>
2023-02-16 19:17:52 +0600

    definitely fixed all

--- LAB3/PART2/21.js
+++ LAB3/PART2/21.js
@@ -12,7 +12,7 @@ function addElements(){
 
     li = `<fieldset class ="box"><li id = "list">
                 <input class = "galochka" unchecked type = "checkbox" id = "item${index}" onclick="cross(this,${index})">
-                <p class = "elem no" id = "cb${index}">${listinput.value}</p>
+                <p style = "text-decoration: none;" class = "elem no" id = "cb${index}">${listinput.value}</p>
                 <button style = "background:transparent;"class = "trash" onclick="deleteitem(${index})">
                    <img style = "position:relative;width:30px;height:30px;left:-9px;top:-5px;"src="https://i.ibb.co/QPhd9LQ/delete.png">  
                 </button>
@@ -63,32 +63,23 @@ function deleteitem(id){
 function cross(cb,id){
     for(var i=0;i<arr.length;++i){
         if(arr[i][1] == id && cb.checked == true){
-            console.log(1)
-            document.getElementById(`cb${id}`).classList.remove('no');
-            document.getElementById(`cb${id}`).classList.add('yes');
-            console.log(arr[i][2])
-            var s = ""
-            s = arr[i][2];
+            var s = arr[i][2];
             var t = s.replace("unchecked","checked");
-            arr[i][2] = t;
-            console.log(arr[i][2])
+            var z = t.replace("none","line-through")
+            arr[i][2] = z;
             document.getElementById(`item${id}`).checked = true;
             cb.checked == false
             arr[i][0]==false;
             break;
         }
         else if(arr[i][1] == id  && cb.checked == false){
-            console.log(0)
-            document.getElementById(`cb${id}`).classList.remove('yes');
-            document.getElementById(`cb${id}`).classList.add('no');
-            console.log(arr[i][2])
-            var s = ""
-            s = arr[i][2];
+            var s = arr[i][2];
             var t = s.replace("checked","unchecked");
-            arr[i][2] = t;
-            console.log(arr[i][2])
+            var z = t.replace("line-through","none")
+            arr[i][2] = z;
             document.getElementById(`item${id}`).checked = false;
             arr[i][0]==true;
+
             break;
         }
     }
